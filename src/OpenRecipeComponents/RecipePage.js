6695d8b52b01bcb175f0d8de5eb0f6e83a1783c7
Fatema Moharam <89610248+moharamfatema@users.noreply.github.com>
2021-09-11 23:32:54 +0200

    added some styling

--- src/OpenRecipeComponents/RecipePage.js
+++ src/OpenRecipeComponents/RecipePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import {Accordion, Button, Card, Col, Container, Image, ListGroup, ListGroupItem, Row, Table} from "react-bootstrap";
-
+import '../App.css'
 class RecipeParent extends React.Component{
     constructor(props) {
         super(props);
@@ -19,92 +19,89 @@ class RecipeParent extends React.Component{
 
 const RecipePage = ({recipe}) =>
     (
-        <Container fluid>
-
+        <Container  className='openRecipe'  fluid>
             <Row>
-                <Col xs={6}>
-                    <Card border="success" bg='light' text='dark'>
-                        <Card.Img variant='top' alt={recipe.label} src={recipe.image}/>
-                        <Card.Title>
-                            {recipe.label}
-                        </Card.Title>
-                        <Button variant='success' onClick={() => window.open(recipe.url)}>Go to recipe</Button>
-                    </Card>
-                </Col>
-                    <Col xs={6}>
-                        <Card>
-                            <Accordion variant='success'>
-                                <Accordion.Item eventKey={0}>
-                                    <Accordion.Header>Cal/serving</Accordion.Header>
-                                    <Accordion.Body>
-                                        {Math.round(recipe.calories/recipe.yield)} kcal
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                <Accordion.Item eventKey={1}>
-                                    <Accordion.Header>Total Time</Accordion.Header>
-                                    <Accordion.Body>
-                                        {recipe.totalTime} minutes
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                <Accordion.Item eventKey={2}>
-                                    <Accordion.Header>Cuisine</Accordion.Header>
-                                    <Accordion.Body>
-                                        {recipe.cuisineType}
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                <Accordion.Item eventKey={3}>
-                                    <Accordion.Header>Meal Type</Accordion.Header>
-                                    <Accordion.Body>
-                                        {recipe.mealType}
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                <Accordion.Item eventKey={4}>
-                                    <Accordion.Header>Dish Type</Accordion.Header>
-                                    <Accordion.Body>
-                                        {recipe.dishType}
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                <Accordion.Item eventKey={5}>
-                                    <Accordion.Header>Servings</Accordion.Header>
-                                    <Accordion.Body>
-                                        {recipe.yield} serving(s)
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                <Accordion.Item eventKey={6}>
-                                    <Accordion.Header>Health Labels</Accordion.Header>
-                                    <Accordion.Body>
-                                        <ListGroup>
-                                            {recipe.healthLabels.map((x,i)=><ListGroup.Item key={i}>{x}</ListGroup.Item>)}
-                                        </ListGroup>
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                                <Accordion.Item eventKey={7}>
-                                    <Accordion.Header>Nutritional facts</Accordion.Header>
-                                    <Accordion.Body>
-                                        <Table bordered hover size='sm'>
-                                            <thead>
-                                            <tr>
-                                                <th>Label</th>
-                                                <th>Quantity</th>
-                                                <th>Unit</th>
+                <Card border="success" bg='light' text='dark'>
+                    <Card.Img variant='top' alt={recipe.label} src={recipe.image} style={{maxHeight:600,maxWidth:600,alignSelf:"center"}}/>
+                    <Card.Title>
+                        {recipe.label}
+                    </Card.Title>
+                    <Button variant='success' onClick={() => window.open(recipe.url)}>Go to recipe</Button>
+                </Card>
+            </Row>
+            <Row >
+                <Card>
+                    <Accordion variant='success'>
+                        <Accordion.Item eventKey={0}>
+                            <Accordion.Header>Cal/serving</Accordion.Header>
+                            <Accordion.Body>
+                                {Math.round(recipe.calories/recipe.yield)} kcal
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey={1}>
+                            <Accordion.Header>Total Time</Accordion.Header>
+                            <Accordion.Body>
+                                {recipe.totalTime} minutes
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey={2}>
+                            <Accordion.Header>Cuisine</Accordion.Header>
+                            <Accordion.Body>
+                                {recipe.cuisineType}
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey={3}>
+                            <Accordion.Header>Meal Type</Accordion.Header>
+                            <Accordion.Body>
+                                {recipe.mealType}
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey={4}>
+                            <Accordion.Header>Dish Type</Accordion.Header>
+                            <Accordion.Body>
+                                {recipe.dishType}
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey={5}>
+                            <Accordion.Header>Servings</Accordion.Header>
+                            <Accordion.Body>
+                                {recipe.yield} serving(s)
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey={6}>
+                            <Accordion.Header>Health Labels</Accordion.Header>
+                            <Accordion.Body>
+                                <ListGroup>
+                                    {recipe.healthLabels.map((x,i)=><ListGroup.Item key={i}>{x}</ListGroup.Item>)}
+                                </ListGroup>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey={7}>
+                            <Accordion.Header>Nutritional facts</Accordion.Header>
+                            <Accordion.Body>
+                                <Table bordered hover size='sm'>
+                                    <thead>
+                                    <tr>
+                                        <th>Label</th>
+                                        <th>Quantity</th>
+                                        <th>Unit</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {Object.keys(recipe.totalNutrients)
+                                        .map((x,i)=>
+                                            <tr key={i}>
+                                                <td>{recipe.totalNutrients[x].label}</td>
+                                                <td>{Math.round(recipe.totalNutrients[x].quantity)}</td>
+                                                <td>{recipe.totalNutrients[x].unit}</td>
                                             </tr>
-                                            </thead>
-                                            <tbody>
-                                            {Object.keys(recipe.totalNutrients)
-                                                .map((x,i)=>
-                                                    <tr key={i}>
-                                                        <td>{recipe.totalNutrients[x].label}</td>
-                                                        <td>{Math.round(recipe.totalNutrients[x].quantity)}</td>
-                                                        <td>{recipe.totalNutrients[x].unit}</td>
-                                                    </tr>
-                                                )}
-                                            </tbody>
-                                        </Table>
-                                    </Accordion.Body>
-                                </Accordion.Item>
-                            </Accordion>
-                        </Card>
-                    </Col>
+                                        )}
+                                    </tbody>
+                                </Table>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                </Card>
             </Row>
         </Container>
     )
