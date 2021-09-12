f1c29eadb76035444db36793c6c870bab1d095ba
nadanabea <nadarabea@gmail.com>
2021-09-12 23:38:27 +0200

    Added SavedButton

--- src/components/SavedButton.js
+++ src/components/SavedButton.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 
 
 
-const SavedButton = () => {
+function SavedButton() {
+
+    const [recipes, setSavedRecipes] = useState([])
+
+    useEffect(() => {
+        const getDate = async () => {
+            const tasksFromServer = await fetchRecipe()
+            setRecipes(tasksFromServer)
+        }
+
+        getDate()
+    }, [])
+
+
+    const fetchRecipe = async (id) => {
+        const rec = await fetch(`http://localhost:3000/recipes/${id}`)
+        const data = await rec.json()
+
+        return data
+    }
+
+    // Add Task
+    const addSaved = async (recipe) => {
+        const rec = await fetch('http://localhost:3000/recipes', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(recipe),
+        })
+
+        const data = await rec.json()
+
+        setSavedRecipes([...recipes, data])
+
+    }
+
     return (
         <div>
-            <Button variant="outline-warning"><i class="far fa-bookmark"></i></Button>
+            <Button variant="outline-warning" onClick={addSaved}><i class="far fa-bookmark"></i></Button>
         </div>
     )
 }
