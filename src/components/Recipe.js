84cd5ef31895c8a8d6d6808f40902ca89ca2ff95
nadanabea <nadarabea@gmail.com>
2021-09-12 11:01:11 +0200

    added homepage w styling

---
+++ src/components/Recipe.js
@@ -0,0 +1,28 @@
+import React from "react";
+import SavedButton from "./SavedButton";
+import { Card } from "react-bootstrap";
+
+
+
+const Recipe = ({ recipe }) => {
+  const { label, image, calories, mealType} = recipe.recipe;
+
+  return (
+    <div className="d-flex justify-content-around">
+  <Card style={{ width: '18rem' }} className="card">
+    <Card.Img variant="top" src={image} />
+    <Card.Body>
+      <Card.Title>{label}</Card.Title>
+      <Card.Text>
+        Calories= {Math.round(calories/5)} <br />
+        Meal: {mealType}
+      </Card.Text>
+      <SavedButton />
+    </Card.Body>
+  </Card>
+  </div>
+
+  );
+};
+
+export default Recipe;
